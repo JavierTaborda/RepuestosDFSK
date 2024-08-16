2d8ab9ef974d4810708a0c65e619db1a3c2bba8b
JavierTaborda <javiert.develop@gmail.com>
2024-08-16 11:30:40 -0400

    Sorting

--- src/pages/RepuestosBodega.jsx
+++ src/pages/RepuestosBodega.jsx
@@ -19,11 +19,12 @@ export default function RepuestosBodega({ addToCart }) {
     const [stringGrupo, setStringGrupo] = useState("*")
     const [stringDescripcion, setstringDescripcion] = useState("*")
     const [stringTextSearch, setstringTextSearch] = useState("")
+    const [orderData, setorderData] = useState("")
 
     const itemsPerPage = 12; // Cambia esto según tus necesidades
     const startIndex = (currentPage - 1) * itemsPerPage;
     var visibleRepuestos = dataRepuesto?.slice(startIndex, startIndex + itemsPerPage);
-   
+
     const toastId = React.useRef(null);//Dont repeat the notification
 
     const notifyerror = (error) => {
@@ -55,48 +56,8 @@ export default function RepuestosBodega({ addToCart }) {
     //         });
     // }, []);
 
-    const filterMarca = async () => {
-        try {
-            setIsLoading(true);
-
-            const URIM = 'http://localhost:5116/api/Articulos/Bodega/Marca/' +
-                encodeURIComponent(stringMarca) + '/' + encodeURIComponent(stringGrupo) 
-                + '/' + encodeURIComponent(stringDescripcion===""?"*":stringDescripcion);
-
-           
-            console.log(URIM);
-
-            const response = await fetch(URIM);
-            if (!response.ok) {
-                notifyerror('Error de la petición.');
-            }
-
-            const dataRepuesto = await response.json();
-            setRepuestos(dataRepuesto);
-            notifysuccess();
-
-            // Resto de las operaciones después de obtener los datos
-            handlePageChange(1);
-            visibleRepuestos = dataRepuesto?.slice(startIndex, startIndex + itemsPerPage);
-        } catch (error) {
-            notifyerror('Error de la petición:'+error.message);
-        } finally {
-            setIsLoading(false);
-           
-        }
-        
-    };
-
     useEffect(() => {
-        if (!isLoading) {
-           
-            filterMarca();
-        }
-       
-    }, [stringMarca, stringGrupo, stringDescripcion]);
 
-    useEffect(() => {
-      
 
         const fetchRepuestos = () => fetch(URI1).then(response => response.json());
         const fetchGrupos = () => fetch(URI2).then(response => response.json());
@@ -113,9 +74,60 @@ export default function RepuestosBodega({ addToCart }) {
                 notifyerror("Error en la carga de datos: " + error.message);
             })
             .finally(() => setIsLoading(false));
-   
+
 
     }, []);
+
+    const filterMarca = async () => {
+        try {
+            setIsLoading(true);
+
+            const URIM = 'http://localhost:5116/api/Articulos/Bodega/Marca/' +
+                encodeURIComponent(stringMarca) + '/' + encodeURIComponent(stringGrupo)
+                + '/' + encodeURIComponent(stringDescripcion === "" ? "*" : stringDescripcion);
+
+
+            console.log(URIM);
+
+            const response = await fetch(URIM);
+            if (!response.ok) {
+                notifyerror('Error de la petición.');
+            }
+
+            const dataRepuesto = await response.json();
+            setRepuestos(dataRepuesto);
+
+            //SORT 
+            dataRepuesto.sort((a, b) => {
+                if (orderData === "Mayor Existencia") { return b.existencia - a.existencia; }
+                else if (orderData === "Menor Existencia") { return a.existencia - b.existencia }
+                else if (orderData === "Mayor Precio") { return b.venta - a.venta }
+                { return a.venta - b.venta }
+            });
+
+
+            //notifysuccess();
+
+            // Paging
+            handlePageChange(1);
+
+            visibleRepuestos = dataRepuesto?.slice(startIndex, startIndex + itemsPerPage);
+        } catch (error) {
+            notifyerror('Error de la petición:' + error.message);
+        } finally {
+            setIsLoading(false);
+
+        }
+
+    };
+
+    useEffect(() => {
+        if (!isLoading) {
+
+            filterMarca();
+        }
+
+    }, [stringMarca, stringGrupo, stringDescripcion, orderData]);
 
 
     const handlePageChange = (pageNumber) => {
@@ -123,16 +135,14 @@ export default function RepuestosBodega({ addToCart }) {
     };
 
 
-
     return (
         <>
-
             <h2 className="bd-title text-center mb-0 pt-2">Inventario de Repuestos</h2>
 
             <div className="container my-5">
 
                 <div className=" d-flex flex-wrap justify-content-between align-items-center p-3">
-               
+
                     <form className="d-flex pe-2 pt-3" role="search">
                         <input className="form-control me-2 rounded-5 shadow-sm" value={stringTextSearch} onChange={(e) => setstringTextSearch(e.target.value)} type="search" placeholder="Buscar..." aria-label="Buscar" />
                         <button className="btn btn-outline-danger rounded-5  shadow-sm" type="button" onClick={() => setstringDescripcion(stringTextSearch)}>Buscar</button>
@@ -167,12 +177,13 @@ export default function RepuestosBodega({ addToCart }) {
 
                     <div className="btn-group ps-2 pt-3" role="group">
                         <button type="button" className="btn btn-outline-danger dropdown-toggle rounded-5 shadow-sm" data-bs-toggle="dropdown" aria-expanded="false">
-                            <i className=" bi bi-sort-down" /> Ordenar por:
+                            <i className=" bi bi-sort-down" /> Ordenar por: {orderData}
                         </button>
                         <ul className="dropdown-menu shadow">
-                            <li><a className="dropdown-item" href="#">Existencia</a></li>
-                            <li><a className="dropdown-item" href="#">Mayor Precio</a></li>
-                            <li><a className="dropdown-item" href="#">Menor Precio</a></li>
+                            <li><a onClick={() => setorderData('Mayor Existencia')} className="dropdown-item" href="#"> <i class="bi bi-sort-down"></i> Mayor Existencia</a></li>
+                            <li><a onClick={() => setorderData('Menor Existencia')} className="dropdown-item" href="#"><i class="bi bi-sort-down-alt"></i> Menor Existencia</a></li>
+                            <li><a onClick={() => setorderData('Mayor Precio')} className="dropdown-item" href="#"> <i class="bi bi-sort-down"></i> Mayor Precio</a></li>
+                            <li><a onClick={() => setorderData('Menor Precio')} className="dropdown-item" href="#"><i class="bi bi-sort-down-alt"></i> Menor Precio</a></li>
                         </ul>
                     </div>
                 </div>
@@ -181,63 +192,64 @@ export default function RepuestosBodega({ addToCart }) {
 
                 {dataRepuesto?.length > 0 ? (
 
-                <div className="d-flex justify-content-center mt-4 mb-2">
-                    <div className="btn-group" role="group" aria-label="Basic mixed styles example">
-                        <button
-                            className="btn btn-outline-danger"
-                            onClick={() => handlePageChange(currentPage - 1)}
-                            disabled={currentPage === 1}
-                        >
-                            Anterior
-                        </button>
-                        <button type="button" className="btn btn-danger"> {currentPage} / {Math.ceil(dataRepuesto?.length / itemsPerPage)}
-                        </button>
+                    <div className="d-flex justify-content-center mt-4 mb-2">
+                        <div className="btn-group" role="group" aria-label="Basic mixed styles example">
+                            <button
+                                className="btn btn-outline-danger"
+                                onClick={() => handlePageChange(currentPage - 1)}
+                                disabled={currentPage === 1}
+                            >
+                                Anterior
+                            </button>
+                            <button type="button" className="btn btn-danger"> {currentPage} / {Math.ceil(dataRepuesto?.length / itemsPerPage)}
+                            </button>
 
-                        <button
-                            className="btn btn-outline-danger"
-                            onClick={() => handlePageChange(currentPage + 1)}
-                            disabled={currentPage === Math.ceil(dataRepuesto?.length / itemsPerPage)}
-                        >
-                            Siguiente
-                        </button>
-                    </div>
-                </div>) : null}
+                            <button
+                                className="btn btn-outline-danger"
+                                onClick={() => handlePageChange(currentPage + 1)}
+                                disabled={currentPage === Math.ceil(dataRepuesto?.length / itemsPerPage)}
+                            >
+                                Siguiente
+                            </button>
+                        </div>
+                    </div>) : null}
 
                 <div className="row">
                     {isLoading ? (
                         <Spinner />
                     ) : (
-                        
+
                         visibleRepuestos.map((item) => (
                             <CardRepuesto key={item.articulo} repuestos={item} addToCart={addToCart} />
                         ))
+
                     )}
                 </div>
-                    
-                { dataRepuesto?.length >0 ?(
-                        
-                   
-                <div className="d-flex justify-content-center mt-4 mb-2">
-                    <div className="btn-group" role="group" aria-label="Basic">
-                        <button
-                            className="btn btn-outline-danger"
-                            onClick={() => handlePageChange(currentPage - 1)}
-                            disabled={currentPage === 1}
-                        >
-                            Anterior
-                        </button>
-                        <button type="button" className="btn btn-danger"> {currentPage} / {Math.ceil(dataRepuesto?.length / itemsPerPage)}
-                        </button>
 
-                        <button
-                            className="btn btn-outline-danger"
-                            onClick={() => handlePageChange(currentPage + 1)}
-                            disabled={currentPage === Math.ceil(dataRepuesto?.length / itemsPerPage)}
-                        >
-                            Siguiente
-                        </button>
-                    </div>
-                </div>): null}
+                {dataRepuesto?.length > 0 ? (
+
+
+                    <div className="d-flex justify-content-center mt-4 mb-2">
+                        <div className="btn-group" role="group" aria-label="Basic">
+                            <button
+                                className="btn btn-outline-danger"
+                                onClick={() => handlePageChange(currentPage - 1)}
+                                disabled={currentPage === 1}
+                            >
+                                Anterior
+                            </button>
+                            <button type="button" className="btn btn-danger"> {currentPage} / {Math.ceil(dataRepuesto?.length / itemsPerPage)}
+                            </button>
+
+                            <button
+                                className="btn btn-outline-danger"
+                                onClick={() => handlePageChange(currentPage + 1)}
+                                disabled={currentPage === Math.ceil(dataRepuesto?.length / itemsPerPage)}
+                            >
+                                Siguiente
+                            </button>
+                        </div>
+                    </div>) : null}
                 <div className="d-flex justify-content-center mt-4 mb-2">
                     {dataRepuesto?.length}  elementos
                 </div>
