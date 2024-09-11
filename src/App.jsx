32d1d523bd5ffd7d87da316e4b01d14c4f0a0f99
JavierTaborda <javiert.develop@gmail.com>
2024-09-11 16:57:15 -0400

    header changes

--- src/App.jsx
+++ src/App.jsx
@@ -63,7 +63,7 @@ function App() {
           />
           <Route path="/solicitudes" element={<EstadosSolicitudes />} />
           <Route path="/vehiculos" element={<Vehicles />} />
-          <Route path="*" element={<Login />} />
+          <Route path="*" element={<Inicio />} />
         </Routes>
         <Footer />
         <ToastContainer position="top-center" draggable />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,7 @@ export default function Header({ cart, removeFromCart, increaseQuantity, decreas
     <>
       <nav className="navbar navbar-expand-lg navbarcolor  sticky-top">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#"><img src="/dfsklogo.png" alt="Logo" className="" width="80" /></a>
+          <Link to="/" className="nav-link active" aria-current="page" href="/"> <img src="/dfsklogo.png" alt="Logo" className="" width="80" /></Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon" />
           </button>
@@ -37,7 +37,7 @@ export default function Header({ cart, removeFromCart, increaseQuantity, decreas
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
 
               <li className="nav-item">
-                <Link to="/inicio" className="nav-link active" aria-current="page" href="/">Inicio</Link>
+                <Link to="/login" className="nav-link active" aria-current="page" href="/">Login</Link>
               </li>
 
               <li className="nav-item dropdown">
