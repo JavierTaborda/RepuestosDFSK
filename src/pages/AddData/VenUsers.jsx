5c16bd4ca7d3630d1cfa42282885f47f0c5eae56
JavierTaborda <javiert.develop@gmail.com>
2024-11-28 15:39:17 -0400

    csss in usrs and login

--- src/pages/AddData/VenUsers.jsx
+++ src/pages/AddData/VenUsers.jsx
@@ -1,10 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import { toast } from 'react-toastify'; // Asegúrate de importar toast
-import 'bootstrap/dist/css/bootstrap.min.css'; // Asegúrate de importar Bootstrap CSS
+import React, { useState, useEffect, useContext } from 'react';
+import { toast } from 'react-toastify';
+import dayjs from 'dayjs';
+import { AuthContext } from '../../context/AuthProvider';
+import { motion } from 'framer-motion';
 import { getDataRoles, postUserData } from '../../services/UserService';
+import Spinner from '../../components/forms/Spinner';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 const VenUsers = () => {
+  const { user } = useContext(AuthContext);
   const [dataRoles, setDataRoles] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
   const [formData, setFormData] = useState({
     codigo: '',
     nombre: '',
@@ -12,8 +18,8 @@ const VenUsers = () => {
     telefono: '',
     clave: '',
     confirmarClave: '',
-    idRol: 0,
-    estatus: true
+    idRol: '',
+    estatus: true,
   });
 
   useEffect(() => {
@@ -23,7 +29,7 @@ const VenUsers = () => {
         const response = await getDataRoles();
         setDataRoles(response);
       } catch (error) {
-        toast.error("Error en la carga de datos: " + error.message);
+        toast.error('Error en la carga de datos: ' + error.message);
       } finally {
         setIsLoading(false);
       }
@@ -36,135 +42,149 @@ const VenUsers = () => {
     const { name, value } = e.target;
     setFormData({
       ...formData,
-      [name]: value
+      [name]: value,
     });
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (formData.clave !== formData.confirmarClave) {
-      toast.error("Las contraseñas no coinciden");
+      toast.error('Las contraseñas no coinciden');
       return;
     }
     try {
       const response = await postUserData(formData);
       console.log('Response:', response);
-      toast.success("Usuario registrado exitosamente");
+      toast.success('Usuario registrado exitosamente');
     } catch (error) {
       console.error('Error:', error);
-      toast.error("Error en el registro: " + error.message);
+      toast.error('Error en el registro: ' + error.message);
     }
   };
 
+  if (!user || isLoading) {
+    return (
+      <div className="d-flex justify-content-center align-items-center" style={{ height: '80vh' }}>
+        <motion.div
+          style={{ fontSize: '18px' }}
+          initial={{ opacity: 1 }}
+          animate={{ opacity: [1, 0, 1] }}
+          transition={{ repeat: Infinity, duration: 1 }}
+        >
+          Cargando datos...
+        </motion.div>
+        <Spinner />
+      </div>
+    );
+  }
+
   return (
     <div className="container mt-5">
-      <h2 className="mb-4">Registrar Usuario</h2>
-      <form onSubmit={handleSubmit} className="row">
-        <div className="form-group mb-3  col-md-6">
-          <label htmlFor="codigo">Rif/Cedula</label>
-          <input
-            type="text"
-            className="form-control"
-            id="codigo"
-            name="codigo"
-            value={formData.codigo}
-            onChange={handleChange}
-            required
-          />
-        </div>
-
-        <div className="form-group mb-3 col-md-6">
-          <label htmlFor="nombre">Nombre</label>
-          <input
-            type="text"
-            className="form-control"
-            id="nombre"
-            name="nombre"
-            value={formData.nombre}
-            onChange={handleChange}
-            required
-          />
-        </div>
-
-        <div className="form-group mb-3 col-md-6">
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            className="form-control"
-            id="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            required
-          />
-        </div>
-
-        <div className="form-group mb-3 col-md-6">
-          <label htmlFor="telefono">Teléfono</label>
-          <input
-            type="text"
-            className="form-control"
-            id="telefono"
-            name="telefono"
-            value={formData.telefono}
-            onChange={handleChange}
-            required
-          />
-        </div>
-
-        <div className="form-group mb-3 col-md-6">
-          <label htmlFor="clave">Clave</label>
-          <input
-            type="password"
-            className="form-control"
-            id="clave"
-            name="clave"
-            value={formData.clave}
-            onChange={handleChange}
-            required
-          />
-        </div>
-
-        <div className="form-group mb-3 col-md-6">
-          <label htmlFor="confirmarClave">Confirmar Clave</label>
-          <input
-            type="password"
-            className="form-control"
-            id="confirmarClave"
-            name="confirmarClave"
-            value={formData.confirmarClave}
-            onChange={handleChange}
-            required
-          />
-        </div>
-
-        {isLoading ? (
-          <div className="spinner-border spinner-border-sm" role="status">
-            <span className="visually-hidden">Loading...</span>
+      <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1 }}>
+        <h2 className="text-center mb-4">Registrar Usuario</h2>
+        <form onSubmit={handleSubmit} className="p-4 rounded shadow bg-white needs-validation" noValidate>
+          <div className="row">
+            <div className="col-md-6 mb-3">
+              <label htmlFor="codigo" className="form-label">Rif/Cedula</label>
+              <input
+                type="text"
+                className="form-control"
+                id="codigo"
+                name="codigo"
+                value={formData.codigo}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div className="col-md-6 mb-3">
+              <label htmlFor="nombre" className="form-label">Nombre</label>
+              <input
+                type="text"
+                className="form-control"
+                id="nombre"
+                name="nombre"
+                value={formData.nombre}
+                onChange={handleChange}
+                required
+              />
+            </div>
           </div>
-        ) : (
-          <div className="form-group mb-3 col-md-6">
-            <label htmlFor="idRol">Rol</label>
-            <select
-              className="form-select"
-              value={formData.idRol}
-              name="idRol"
-              onChange={handleChange}
-              required
-            >
-              <option value="">Seleccione un rol</option>
-              {dataRoles.map((item) => (
-                <option key={item.idRol} value={item.idRol}>
-                  {item.rolName}
-                </option>
-              ))}
-            </select>
+          <div className="row">
+            <div className="col-md-6 mb-3">
+              <label htmlFor="email" className="form-label">Email</label>
+              <input
+                type="email"
+                className="form-control"
+                id="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div className="col-md-6 mb-3">
+              <label htmlFor="telefono" className="form-label">Teléfono</label>
+              <input
+                type="text"
+                className="form-control"
+                id="telefono"
+                name="telefono"
+                value={formData.telefono}
+                onChange={handleChange}
+                required
+              />
+            </div>
           </div>
-        )}
-
-        <button type="submit" className="btn btn-success mt-3"> <i className="bi bi-pencil"></i> Registrar</button>
-
-      </form>
+          <div className="row">
+            <div className="col-md-6 mb-3">
+              <label htmlFor="clave" className="form-label">Clave</label>
+              <input
+                type="password"
+                className="form-control"
+                id="clave"
+                name="clave"
+                value={formData.clave}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div className="col-md-6 mb-3">
+              <label htmlFor="confirmarClave" className="form-label">Confirmar Clave</label>
+              <input
+                type="password"
+                className="form-control"
+                id="confirmarClave"
+                name="confirmarClave"
+                value={formData.confirmarClave}
+                onChange={handleChange}
+                required
+              />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-12 mb-3">
+              <label htmlFor="idRol" className="form-label">Rol</label>
+              <select
+                className="form-select"
+                value={formData.idRol}
+                name="idRol"
+                onChange={handleChange}
+                required
+              >
+                <option value="">Seleccione un rol</option>
+                {dataRoles.map((item) => (
+                  <option key={item.idRol} value={item.idRol}>
+                    {item.rolName}
+                  </option>
+                ))}
+              </select>
+            </div>
+          </div>
+          <button type="submit" className="btn btn-danger mt-3 w-100">
+            Registrar
+          </button>
+        </form>
+      </motion.div>
     </div>
   );
 };
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,9 @@
 import React, { useState, useContext } from 'react';
 import { toast } from 'react-toastify';
+import { motion } from 'framer-motion';
 import Carousel from '../components/forms/Carousel';
 import { AuthContext } from '../context/AuthProvider';
+
 const Login = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
@@ -21,70 +23,73 @@ const Login = () => {
         setError(null);
 
         try {
-            
             await login({ username, password }).then((response) => {
                 toast.success(response);
             });
-            
         } catch (error) {
             setError('Falló el inicio de sesión, por favor verifique sus credenciales.');
             console.log(error);
             toast.error(error.request.response);
         } finally {
-
-            setLoading(false);      
+            setLoading(false);
         }
     };
 
-
     return (
-        <div className='container pt-5 pb-4'>
-            <div className='row align-items-center'>
-
-                <div className='col-md-4 '>
-                    <div className="login-container mx-auto">
-                        <img src="/DFSKimg.png" alt="Logo" className="p-4" width="175" />
-                        <h2>Iniciar Sesión</h2>
-                        <form onSubmit={handleSubmit}>
-                            <div>
-                                <label htmlFor="username">Usuario:</label>
-                                <input
-                                    type="text"
-                                    id="username"
-                                    value={username}
-                                    onChange={(e) => setUsername(e.target.value)}
-                                    required
-                                />
-                            </div>
-                            <div className="password-container">
-                                <label htmlFor="password">Contraseña:</label>
-                                <input
-                                    type={showPassword ? 'text' : 'password'}
-                                    id="password"
-                                    value={password}
-                                    onChange={(e) => setPassword(e.target.value)}
-                                    required
-                                />
-                                <span
-                                    className="toggle-password"
-                                    onClick={togglePasswordVisibility}
-                                >
-                                    {showPassword ? <i className="bi bi-eye-slash"></i> : <i className="bi bi-eye"></i>}
-                                </span>
-                            </div>
-                            {error && <p className="error">{error}</p>}
-                            <button type="submit" className="btn btn-success" disabled={loading}>
-                                {loading ? 'Validando...' : <><i className="bi bi-box-arrow-in-left"></i> Iniciar Sesión</>}
-                            </button>
-                        </form>
+        <div className='container mt-5'>
+            <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1 }}>
+                <div className='row align-items-center'>
+                    <div className='col-md-6'>
+                        <motion.div
+                            initial={{ x: -100, opacity: 0 }}
+                            animate={{ x: 0, opacity: 1 }}
+                            transition={{ duration: 0.5 }}
+                            className="login-container p-4 rounded shadow bg-white mx-auto"
+                        >
+                            <img src="/DFSKimg.png" alt="Logo" className="mb-4" width="175" />
+                            <h2 className="text-center mb-4">Iniciar Sesión</h2>
+                            <form onSubmit={handleSubmit}>
+                                <div className="mb-3">
+                                    <label htmlFor="username" className="form-label">Usuario:</label>
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        id="username"
+                                        value={username}
+                                        onChange={(e) => setUsername(e.target.value)}
+                                        required
+                                    />
+                                </div>
+                                <div className="mb-3 position-relative">
+                                    <label htmlFor="password" className="form-label">Contraseña:</label>
+                                    <input
+                                        type={showPassword ? 'text' : 'password'}
+                                        className="form-control"
+                                        id="password"
+                                        value={password}
+                                        onChange={(e) => setPassword(e.target.value)}
+                                        required
+                                    />
+                                    <span
+                                        className="toggle-password position-absolute top-50 end-0 translate-middle-y pe-3"
+                                        onClick={togglePasswordVisibility}
+                                        style={{ cursor: 'pointer' }}
+                                    >
+                                        {showPassword ? <i className="bi bi-eye-slash"></i> : <i className="bi bi-eye"></i>}
+                                    </span>
+                                </div>
+                                {error && <p className="text-danger">{error}</p>}
+                                <button type="submit" className="btn btn-danger w-100" disabled={loading}>
+                                    {loading ? 'Validando...' : <><i className="bi bi-box-arrow-in-left"></i> Iniciar Sesión</>}
+                                </button>
+                            </form>
+                        </motion.div>
+                    </div>
+                    <div className='col-md-6'>
+                        <Carousel />
                     </div>
                 </div>
-                <div className='col-md-8 mx-auto'>
-
-                    <Carousel />
-
-                </div>
-            </div>
+            </motion.div>
         </div>
     );
 };
